2c878c461f98925b704a9c9f2fccdd2214158656
LQ458 <143152892+LQ458@users.noreply.github.com>
2023-12-11 23:38:07 +0800

    dashboard change style

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,8 +6,6 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return (
-        <Dashboardform />
-    );
+    return <Dashboardform />;
   }
 }
