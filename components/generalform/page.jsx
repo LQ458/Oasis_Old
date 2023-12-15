d17bb3581119acf8cb59feff8baa79b1593b1ea5
LQ <yihao.qin17311-biph@basischina.com>
2023-12-15 12:13:16 +0000

    change

--- components/generalform/page.jsx
+++ components/generalform/page.jsx
@@ -80,7 +80,7 @@ function generalform({ admin }) {
     try {
       setLoad(true);
       const res = await Promise.race([
-        axios.post("http://oasisforum.cn:3001/upload", formData, {
+        axios.post("http://45.145.229.105:3001/upload", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
