af8e83bd5491cd36682c1ea8a60b3c090b888e8e
LQ <143152892+LQ458@users.noreply.github.com>
2024-01-17 00:02:47 +0800

    confession

--- app/confession/page.jsx
+++ app/confession/page.jsx
@@ -6,6 +6,6 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return <Confessionform />;
+    return <Confessionform username={session?.user?.name}/>;
   }
 }
--- components/confessionform.jsx
+++ components/confessionform.jsx
@@ -4,7 +4,12 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import styles from "../app/src/confession.scss";
 
-export default function Confessionform() {
+export default function Confessionform({username}) {
+  const [toWho, setToWho] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [content, setContent] = useState("");
+  const [anonymous, setAnonymous] = useState(true);
+
   const getLove = async () => {
     try{
       const response = await axios.get("/api/fetchLove");
@@ -22,12 +27,35 @@ export default function Confessionform() {
     fetchLove();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await axios.post("/api/fetchLove", {
+        username,
+        toWho,
+        nickname,
+        content,
+        anonymous,
+      });
+      console.log(response);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <body className="loveBg">
       <div className="pyro">
         <div className="before"></div>
         <div className="after"></div>
       </div>
+      <form className="loveForm" onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
+        <input type="text" name="toWho" required placeholder="To who?" value={toWho} onChange={(e) => setToWho(e.target.value)} />
+        <input type="text" name="nickname" placeholder="Your nickname?" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+        <textarea name="content" placeholder="Your confession?" required value={content} onChange={(e) => setContent(e.target.value)} />
+        <input type="checkbox" name="anonymous" value={anonymous} onChange={(e) => setAnonymous(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
     </body>
   );
 }
