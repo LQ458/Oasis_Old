c71d7893d229c46d3e1ef73775c5dd409c374543
LQ <143152892+LQ458@users.noreply.github.com>
2024-01-23 23:32:30 +0800

    Like Initial Setup

--- app/api/fetchLike/route.js
+++ app/api/fetchLike/route.js
@@ -4,33 +4,43 @@ import Likestatus from "@/models/likestatus";
 import DBconnect from "@/app/libs/mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET(req, res) {
-  await DBconnect();
-  const likes = await Post.find({ forum: "general" });
-  return NextResponse.json({ likes }, { status: 200 });
+export async function GET(req) {
+  try {
+    await DBconnect();
+    const username = req.nextUrl.searchParams.get('username');
+    const [likes, likestatuses] = await Promise.all([
+      Like.find({ forum: "general" }),
+      Likestatus.find({ username: username }),
+    ]);
+    console.log(username);
+    return NextResponse.json({ likes, likestatuses }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server error' }, { status: 500 });
+  }
 }
 
-export async function POST(req, res) {
+export async function POST(req) {
   await DBconnect();
   const { postId, sendUsername, status } = await req.json();
-  if (status) {
-    await Like.findOneAndUpdate(
-      { postId: postId },
-      { $inc: { number: 1 } },
-      { new: true },
-    );
-  } else {
-    await Like.findOneAndUpdate(
-      { postId: postId },
-      { $inc: { number: -1 } },
-      { new: true },
-    );
-  }
-  await Likestatus.findOneAndUpdate(
-    { postId: postId, username: sendUsername },
-    { $set: { status: status } },
-    { upsert: true, new: true },
-  );
+  await Promise.all([
+    status 
+      ? Like.findOneAndUpdate(
+          { postId: postId },
+          { $inc: { number: 1 } },
+          { new: true },
+        )
+      : Like.findOneAndUpdate(
+          { postId: postId },
+          { $inc: { number: -1 } },
+          { new: true },
+        ),
+    Likestatus.findOneAndUpdate(
+      { postId: postId, username: sendUsername },
+      { $set: { status: status } },
+      { upsert: true, new: true },
+    ),
+  ]);
   return NextResponse.json(
     { message: "like successfully recorded" },
     { status: 201 },
