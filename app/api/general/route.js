0b0c2bd25b0d35a4d30dcf46c6878edf346696f4
LQ <143152892+LQ458@users.noreply.github.com>
2024-01-24 21:37:36 +0800

    file disposal

--- app/api/general/route.js
+++ app/api/general/route.js
@@ -1,6 +1,8 @@
 import DBconnect from "@/app/libs/mongodb";
 import Post from "@/models/post";
 import Like from "@/models/like";
+import fs from "fs";
+import path from "path";
 import Likestatus from "@/models/likestatus";
 import { NextResponse } from "next/server";
 
@@ -14,9 +16,18 @@ export async function GET() {
 export async function DELETE(req) {
   const { id } = await req.json();
   await DBconnect();
+  const post = await Post.findById(id);
+  if (post && post.pictureUrl && post.pictureUrl.path) {
+    try {
+      fs.unlink(path.join(__dirname, post.pictureUrl.path));
+      fs.unlink(path.join(__dirname, post.pictureUrl.filename));
+    } catch (err) {
+      console.error(`Failed to delete local image: ${err}`);
+    }
+  }
   Promise.all([
     Post.findByIdAndDelete(id),
-    Like.findOneAndDelete({ postId: id }),
+    Like.findByIdAndDelete(id),
     Likestatus.deleteMany({ postId: id }),
   ]);
   return NextResponse.json({ message: "Post deleted" }, { status: 200 });
