9e523af2a50b8ed5c43f05558bd0fcca895a8d88
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-01 17:16:49 +0800

    channels style

--- app/discussion/page.jsx
+++ app/discussion/page.jsx
@@ -1,14 +1,14 @@
-import DiscussionForm from "@/components/discussionform.jsx";
+import DiscussionForm from "@/components/discussionForm.jsx";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import axios from "axios";
 import { authOptions } from "../api/auth/[...nextauth]/route.js";
 
-export default async function discussion() {
+export default async function Discussion() {
   let admin;
   const session = await getServerSession(authOptions);
 
-  if(!session) return redirect("/");
+  if(!session) redirect("/");
 
   try {
     const res = await axios.post("http://localhost:3000/api/fetchAdmin", {
