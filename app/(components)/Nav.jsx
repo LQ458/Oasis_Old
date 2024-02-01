b52832f7f6bada3b4f90e55dfbcf0c9b678ebfd0
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-01 17:34:08 +0800

    side bar as nav

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,5 +1,116 @@
+'use client'
+import Link from "next/link";
+import $ from "jquery";
+import { useEffect } from "react";
+import Image from "next/image";
+import "../src/nav.css";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  useEffect(() => {
+    var channels = $(".channel");
+    for (var i = 0; i < channels.length; i++) {
+      $(channels[i]).css("animation-delay", i * 0.2 + "s");
+    }
+
+    let liftInterval;
+
+    $(function () {
+      $(".channel").hover(
+        function () {
+          var iconClass = $(this).data("icon");
+          $(".icon").removeClass("lift");
+          $("." + iconClass).addClass("lift");
+        },
+        function () {
+          $(".icon").removeClass("lift");
+        },
+      );
+    });
+    $(".channel").click(
+      function () {
+        var iconClass = $(this).data("icon");
+        $(".icon").removeClass("lift");
+        liftInterval = setInterval(() => {
+          $("." + iconClass).toggleClass("lift");
+        }, 500);
+      },
+      function () {
+        clearInterval(liftInterval);
+        $(".icon").removeClass("lift");
+      },
+    );
+  }, []);
+  return (
+    <nav className="dash">
+        <div className="container">
+          <div className="dblock">
+            <Link href="/profile" className="icon">
+              <Image
+                src="/person-circle-outline.svg"
+                width="60"
+                height="60"
+                alt="person"
+              />
+            </Link>
+          </div>
+          <div className="dblock">
+            <div className="icon icon-2">
+              <Image
+                src="/earth-outline.svg"
+                width="40"
+                height="40"
+                alt="earth"
+              />
+            </div>
+            <li>
+              <Link className="channel" data-icon="icon-2" href="/general">
+                General
+              </Link>
+            </li>
+          </div>
+          <div className="dblock">
+            <div className="icon icon-3">
+              <Image
+                src="/chatbox-outline.svg"
+                width="40"
+                height="40"
+                alt="chatbox"
+              />
+            </div>
+            <li>
+              <Link className="channel" data-icon="icon-3" href="/news">
+                News
+              </Link>
+            </li>
+          </div>
+          <div className="dblock">
+            <div className="icon icon-4">
+              <Image src="/heart.svg" width="40" height="40" alt="heart" />
+            </div>
+            <li>
+              <Link className="channel" data-icon="icon-4" href="/confession">
+                Confession
+              </Link>
+            </li>
+          </div>
+          <div className="dblock">
+            <div className="icon icon-5">
+              <Image
+                src="/book-outline.svg"
+                width="40"
+                height="40"
+                alt="book"
+              />
+            </div>
+            <li>
+              <Link href="/discussion" className="channel" data-icon="icon-5">
+                Discussion
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+  );
 };
 
 export default Nav;
--- components/dashboardform/page.jsx
+++ components/dashboardform/page.jsx
@@ -1,10 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useEffect } from "react";
-import $ from "jquery";
+import Nav from "@/app/(components)/Nav";
 import { useState } from "react";
 import axios from "axios";
-import Link from "next/link";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "../../app/src/dashboard.css";
 
@@ -110,115 +109,12 @@ export default function Dashboardform({ username }) {
     getPosts();
   }, []);
 
-  useEffect(() => {
-    var channels = $(".channel");
-    for (var i = 0; i < channels.length; i++) {
-      $(channels[i]).css("animation-delay", i * 0.2 + "s");
-    }
-
-    let liftInterval;
-
-    $(function () {
-      $(".channel").hover(
-        function () {
-          var iconClass = $(this).data("icon");
-          $(".icon").removeClass("lift");
-          $("." + iconClass).addClass("lift");
-        },
-        function () {
-          $(".icon").removeClass("lift");
-        },
-      );
-    });
-    $(".channel").click(
-      function () {
-        var iconClass = $(this).data("icon");
-        $(".icon").removeClass("lift");
-        liftInterval = setInterval(() => {
-          $("." + iconClass).toggleClass("lift");
-        }, 500);
-      },
-      function () {
-        clearInterval(liftInterval);
-        $(".icon").removeClass("lift");
-      },
-    );
-  }, []);
   return (
     <body className="dash">
       <title>Dashboard</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <nav className="dash">
-        <div className="container">
-          <div className="dblock">
-            <Link href="/profile" className="icon">
-              <Image
-                src="/person-circle-outline.svg"
-                width="60"
-                height="60"
-                alt="person"
-              />
-            </Link>
-          </div>
-          <div className="dblock">
-            <div className="icon icon-2">
-              <Image
-                src="/earth-outline.svg"
-                width="40"
-                height="40"
-                alt="earth"
-              />
-            </div>
-            <li>
-              <Link className="channel" data-icon="icon-2" href="/general">
-                General
-              </Link>
-            </li>
-          </div>
-          <div className="dblock">
-            <div className="icon icon-3">
-              <Image
-                src="/chatbox-outline.svg"
-                width="40"
-                height="40"
-                alt="chatbox"
-              />
-            </div>
-            <li>
-              <Link className="channel" data-icon="icon-3" href="/news">
-                News
-              </Link>
-            </li>
-          </div>
-          <div className="dblock">
-            <div className="icon icon-4">
-              <Image src="/heart.svg" width="40" height="40" alt="heart" />
-            </div>
-            <li>
-              <Link className="channel" data-icon="icon-4" href="/confession">
-                Confession
-              </Link>
-            </li>
-          </div>
-          <div className="dblock">
-            <div className="icon icon-5">
-              <Image
-                src="/book-outline.svg"
-                width="40"
-                height="40"
-                alt="book"
-              />
-            </div>
-            <li>
-              <Link href="/discussion" className="channel" data-icon="icon-5">
-                Discussion
-              </Link>
-            </li>
-          </div>
-        </div>
-      </nav>
-      <br />
+      <Nav />
       <section className="secd">
         <h1 className="dashh1">Welcome to {username}&apos;s Dashboard!</h1>
         <div className="comments">
