aa1d062584d56e1d22a91608645e556ee7f049d4
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-10 23:21:33 +0800

    Comment Data Transferring
    Both backend api endpoints and frontend data preserving (saving)

--- app/api/fetchComments/route.js
+++ app/api/fetchComments/route.js
@@ -1,7 +1,21 @@
 import Comment from "@/models/comment";
 import Post from "@/models/post";
 import DBconnect from "@/libs/mongodb";
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  await DBconnect();
+  try {
+    await DBconnect();
+    const postId = req.nextUrl.searchParams.get("postId");
+    const comments = (await Comment.find({ postId: postId })).reverse();
+    return NextResponse.json(
+      { comments, message: "Successfully fetched comments" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: error, message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
 }
