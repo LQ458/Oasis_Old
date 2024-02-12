63f68534abe0840568aaf1e0dfe38c75ff77b989
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-12 22:14:46 +0800

    Comments V1

--- app/api/fetchComments/route.js
+++ app/api/fetchComments/route.js
@@ -19,3 +19,20 @@ export async function GET(req) {
     );
   }
 }
+
+export async function POST(req) {
+  try {
+    await DBconnect();
+    const { postId } = await req.json();
+    const commentNumber = await Comment.countDocuments({ postId: postId });
+    return NextResponse.json(
+      { commentNumber, message: "Successfully fetched comment number" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: error, message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
